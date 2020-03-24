7b3aa5aff74e171127c15bc5cad905d64339cb62
MJ <mjw@majiawei.com>
2020-03-25 00:07:21 +0800

    feat(main): 增加常用的复合类型的支持
    新增支持存储/读取的复合类型 []int, []string, map[string]int, map[string]string

--- config.go
+++ config.go
@@ -1,6 +1,7 @@
 package reborn
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
 )
@@ -28,6 +29,22 @@ func (c Config) valueToString(value interface{}) (string, error) {
 		break
 	case bool:
 		v = fmt.Sprintf("%v", value.(bool))
+		break
+	case []int:
+		j, _ := json.Marshal(value.([]int))
+		v = string(j)
+		break
+	case []string:
+		j, _ := json.Marshal(value.([]string))
+		v = string(j)
+		break
+	case map[string]int:
+		j, _ := json.Marshal(value.(map[string]int))
+		v = string(j)
+		break
+	case map[string]string:
+		j, _ := json.Marshal(value.(map[string]string))
+		v = string(j)
 		break
 	default:
 		return "", UnsupportedValueTypeError{value}
@@ -85,4 +102,56 @@ func (c Config) GetBoolValue(key string, defaults bool) bool {
 		return defaults
 	}
 	return v
+}
+
+func (c Config) GetIntSliceValue(key string, defaults []int) []int {
+	vStr := c.GetValue(key, "")
+	if vStr == "" {
+		return defaults
+	}
+	r := make([]int, 0)
+	err := json.Unmarshal([]byte(vStr), &r)
+	if err != nil {
+		return defaults
+	}
+	return r
+}
+
+func (c Config) GetStringSliceValue(key string, defaults []string) []string {
+	vStr := c.GetValue(key, "")
+	if vStr == "" {
+		return defaults
+	}
+	r := make([]string, 0)
+	err := json.Unmarshal([]byte(vStr), &r)
+	if err != nil {
+		return defaults
+	}
+	return r
+}
+
+func (c Config) GetStringIntMapValue(key string, defaults map[string]int) map[string]int {
+	vStr := c.GetValue(key, "")
+	if vStr == "" {
+		return defaults
+	}
+	r := make(map[string]int)
+	err := json.Unmarshal([]byte(vStr), &r)
+	if err != nil {
+		return defaults
+	}
+	return r
+}
+
+func (c Config) GetStringStringMapValue(key string, defaults map[string]string) map[string]string {
+	vStr := c.GetValue(key, "")
+	if vStr == "" {
+		return defaults
+	}
+	r := make(map[string]string)
+	err := json.Unmarshal([]byte(vStr), &r)
+	if err != nil {
+		return defaults
+	}
+	return r
 }
